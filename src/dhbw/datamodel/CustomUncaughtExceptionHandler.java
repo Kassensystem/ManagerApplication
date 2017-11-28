5c5a131ae171cf14d028d20a96e278c49157a914
marvinmai <marvinmai@posteo.de>
2017-11-28 23:15:10 +0100

    Exception-Logging optimiert

--- src/dhbw/datamodel/CustomUncaughtExceptionHandler.java
+++ src/dhbw/datamodel/CustomUncaughtExceptionHandler.java
@@ -1,6 +1,7 @@
 package dhbw.datamodel;
 
 import dhbw.AlertBox;
+import dhbw.KassensystemManagerController;
 import dhbw.sa.kassensystem_rest.exceptions.DataException;
 import dhbw.sa.kassensystem_rest.exceptions.MySQLServerConnectionException;
 import dhbw.sa.kassensystem_rest.exceptions.NoContentException;
@@ -15,10 +16,16 @@ public class CustomUncaughtExceptionHandler implements Thread.UncaughtExceptionH
 
     private String filename = "errorlog.txt";
 
+    /**
+     * Alle Exceptions des {@link KassensystemManagerController} werden hier abgefangen.
+     * Die Exceptions {@link DataException}, {@link MySQLServerConnectionException}, {@link NoContentException},
+     * und {@link OrderNotFoundException} werden in einer {@link AlertBox} dem Nutzer dargestellt. Außerdem werden
+     * diese und alle anderen Exceptions in einer logging-Datei mit dem Namen {@param filename} abgespeichert.
+     * @param t
+     * @param e
+     */
     @Override
     public void uncaughtException(Thread t, Throwable e) {
-        //Die Fehlermeldung in eine logging Datei speichern
-        String date = DateTime.now().toString("dd.MM.yyyy kk:mm:ss");
 
         try {
             ArrayList<String> logging = null;
@@ -29,6 +36,7 @@ public class CustomUncaughtExceptionHandler implements Thread.UncaughtExceptionH
                     logging.add(reader.readLine());
                 }
             } catch (IOException e1) {
+                //datei existiert noch nicht, also auch kein zu speichernder Inhalt
                 e1.printStackTrace();
             }
 
@@ -40,39 +48,43 @@ public class CustomUncaughtExceptionHandler implements Thread.UncaughtExceptionH
                 }
             }
 
-            Throwable ex = getCause(e);
+            Throwable rootCause = getRootCause(e);
 
+            String loggingText = rootCause.getMessage();
+
+            String date = DateTime.now().toString("dd.MM.yyyy kk:mm:ss");
+            logger.write(date + "   " + loggingText);
+
+            //ermitteln des StackTraces als String
             StringWriter sw = new StringWriter();
             PrintWriter pw = new PrintWriter(sw);
-            ex.printStackTrace(pw);
-            String sStackTrace = sw.toString(); // stack trace as a string
+            rootCause.printStackTrace(pw);
+            String stackTraceString = sw.toString();
 
-            String loggingText = ex.getMessage();
             logger.newLine();
-            logger.write(sStackTrace);
-
-            logger.write(date + "   " + loggingText);
+            logger.write(stackTraceString);
 
             logger.close();
 
-            if(ex instanceof DataException
-                    || ex instanceof MySQLServerConnectionException
-                    || ex instanceof OrderNotFoundException
-                    || ex instanceof NoContentException)
-                AlertBox.display("Error", ex.getMessage());
+            if(rootCause instanceof DataException
+                    || rootCause instanceof MySQLServerConnectionException
+                    || rootCause instanceof OrderNotFoundException
+                    || rootCause instanceof NoContentException)
+                AlertBox.display("Error", rootCause.getMessage());
             else
-                AlertBox.display("DEBUGGING ERROR", "DEBUGGING ERROR\n" + ex.getMessage());
+                AlertBox.display("DEBUGGING ERROR", "DEBUGGING ERROR\n" + rootCause.getMessage());
 
         } catch (IOException e1) {
             e1.printStackTrace();
         }
-
-        //AlertBox alertBox = new AlertBox();
-
-        //alertBox.display("Error", e.getCause().getMessage() + "\n" + e.getCause().getStackTrace().toString());
     }
 
-    Throwable getCause(Throwable e) {
+    /**
+     * Ermittelt den ursprünglichen Grund (RootCause) für eine Exception.
+     * @param e Exception, deren RootCause ermittelt werden soll.
+     * @return {@link Throwable} RootCause der Exception e.
+     */
+    Throwable getRootCause(Throwable e) {
         Throwable cause = null;
         Throwable result = e;
 
